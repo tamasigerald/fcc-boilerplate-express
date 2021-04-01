1971f83fb9e0345684e94158ef26e80bf0e1c215
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-04-01 14:11:07 +0200

    changes

--- myApp.js
+++ myApp.js
@@ -1,39 +1,20 @@
 var express = require('express');
 var app = express();
 
+require('dotenv').config();
+
+console.log('Hello World');
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/json', function(req, res) {
+    let response = process.env.MESSAGE_STYLE === 'uppercase' ? "Hello World".toUpperCase() : "Hello World";
+    res.json({message: `${response}`})
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
